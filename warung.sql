50099ef770d5acc08dfa0d4ace21e704c9de3349
unknown <ahmadirvan9c17@gmail.com>
2023-06-27 15:06:24 +0700

    add new files

---
+++ warung.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 27, 2023 at 10:03 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `warung`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `warung`
+--
+
+CREATE TABLE `warung` (
+  `NAMA` varchar(50) NOT NULL,
+  `MAKANAN` varchar(50) NOT NULL,
+  `MINUMAN` varchar(50) NOT NULL,
+  `CATATAN` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `warung`
+--
+
+INSERT INTO `warung` (`NAMA`, `MAKANAN`, `MINUMAN`, `CATATAN`) VALUES
+('', '', '', '');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
